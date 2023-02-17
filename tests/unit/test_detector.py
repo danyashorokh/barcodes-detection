a0597d27606c651ac6799e602bb49018af3a43c6
Danila Shorokh <shorokhdk@sibur.ru>
2023-02-17 12:15:28 +0400

    fix tests

--- tests/unit/test_detector.py
+++ tests/unit/test_detector.py
@@ -7,13 +7,11 @@ import numpy as np
 def test_model_empty_tensor(model_wrapper, dummy_input):
     boxes, scores, labels_ids = model_wrapper.detect(dummy_input)
     assert scores
-    assert labels_ids
 
 
 def test_model_np_image(model_wrapper, sample_image_np):
     boxes, scores, labels_ids = model_wrapper.detect(sample_image_np)
     assert scores
-    assert labels_ids
 
 
 def test_correct_probs(model_wrapper, dummy_input, sample_image_np):
@@ -23,6 +21,9 @@ def test_correct_probs(model_wrapper, dummy_input, sample_image_np):
     for score in scores:
         assert score <= 1
         assert score >= 0
+
+    for labels_id in labels_ids:
+        assert labels_id in model_wrapper.classes.keys()
 
 
 def test_predict_dont_mutate_orig_image(model_wrapper, sample_image_np):
