d952d6157047f0739b7c1aed5f84575fbe66b32e
Danila Shorokh <shorokhdk@sibur.ru>
2023-02-17 12:36:01 +0400

    fix tests

--- tests/unit/test_detector.py
+++ tests/unit/test_detector.py
@@ -6,12 +6,12 @@ import numpy as np
 
 def test_model_empty_tensor(model_wrapper, dummy_input):
     boxes, scores, labels_ids = model_wrapper.detect(dummy_input)
-    assert not scores
+    assert not scores, scores
 
 
 def test_model_np_image(model_wrapper, sample_image_np):
     boxes, scores, labels_ids = model_wrapper.detect(sample_image_np)
-    assert scores
+    assert scores, scores
 
 
 def test_correct_probs(model_wrapper, dummy_input, sample_image_np):
@@ -19,8 +19,8 @@ def test_correct_probs(model_wrapper, dummy_input, sample_image_np):
     boxes, scores, labels_ids = model_wrapper.detect(dummy_input)
 
     for score in scores:
-        assert score <= 1
-        assert score >= 0
+        assert score <= 1, score
+        assert score >= 0, score
 
     for labels_id in labels_ids:
         assert labels_id in model_wrapper.classes.keys()
